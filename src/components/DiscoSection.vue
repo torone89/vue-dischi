b2f658ddb6b61f2daa450aaeea7d62f774868ef6
torone89 <torone89>
2022-07-26 14:21:02 +0200

    Add scc Foler e Several Components

---
+++ src/components/DiscoSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <!-- COMPONENTE GENERATE DISCO E LOGICA V-FOR -->
+    <generate-disco />
+  </div>
+</template>
+
+<script>
+import GenerateDisco from "./GenerateDisco.vue";
+export default {
+  name: "DiscoSection",
+  components: {
+    GenerateDisco,
+  },
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
